c339bb08719fbd6a395312ce67d819dd8a35368a
Artem Vasylenko <irbiskronos@gmail.com>
2023-01-17 23:28:47 +0200

    Begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { Block } from './Block';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json);
+        console.log(json);
+      })
+      .catch((err) => {
+        alert('Не вдалося отримати курси валют');
+        console.warn(err);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Block value={0} currency="UAH" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
